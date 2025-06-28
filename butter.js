5a80366007cb6ca1de9e01278c7533ef66102a03
priya-singh27 <priya.beingdev@gmail.com>
2025-06-28 22:20:34 +0530

    error handling middleware

--- butter.js
+++ butter.js
@@ -2,67 +2,85 @@ const http =require('node:http');
 const fs= require('node:fs/promises')
 
 class Butter{
-
     constructor(){
         this.server = http.createServer();
         this.routes = {}
         this.middleware = [];
+        this.handleErr;
 
         this.server.on('request', (req,res)=>{
+          res.status = (code) => {
+            //set status code of response
+            res.statusCode = code;
+            return res;
+          };
 
-            res.status = (code) => {//set status code of response
-                res.statusCode = code;
-                return res;
-            }
+          res.json = (data) => {
+            //send json data to the client, less than highWaterMark value
+            res.setHeader("content-type", "application/json");
+            res.end(JSON.stringify(data));
+          };
 
-            res.json = (data)=>{//send json data to the client, less than highWaterMark value
-                res.setHeader("content-type", "application/json");
-                res.end(JSON.stringify(data));
-            }
-            
-            res.uploadFile = async (folder)=>{
-                const mime = req.headers['content-type']
-                const fileType = mime.split('/').pop();
+          res.uploadFile = async (folder) => {
+            const mime = req.headers["content-type"];
+            const fileExtension = mime.split("/").pop();
 
-                const fileName = `file_${Date.now()}_${Math.floor(Math.random() * 1e6)}.${fileType}`;
-                const fileHandler = await fs.open(`./${folder}/${fileName}`, 'w');
-                const writeStream = fileHandler.createWriteStream();
-                
-                req.pipe(writeStream);
-                
-                res.setHeader("content-type", mime);
-                
-                req.on('end', ()=>{//end event: when the reading is done
-                    res.end(
-                        JSON.stringify({message : "File uploaded successfully"})
-                    );
-                });
-            }
+            const fileName = `file_${Date.now()}_${Math.floor(
+              Math.random() * 1e6
+            )}.${fileExtension}`;
+            const fileHandler = await fs.open(`./${folder}/${fileName}`, "w");
+            const writeStream = fileHandler.createWriteStream();
 
-            res.sendFile = async (path, mimeType)=>{
-                const fileHandler = await fs.open(path,'r');
-                const readStream = fileHandler.createReadStream();
-                res.setHeader('content-type', mimeType)
-                readStream.pipe(res);
-            }
+            req.pipe(writeStream);
 
-            const runMiddleware=(req, res, middleware_array, idx)=>{
-                if(idx === middleware_array.length){
+            res.setHeader("content-type", mime);
 
-                    //if the routes obj does not have the key of req.method+req.url
-                    if(!this.routes[req.method.toLowerCase()+req.url]){
-                        return res.status(404).json({error: `Cannot ${req.method} ${req.url}`})
-                    }
+            req.on("end", () => {
+              //end event: when the reading is done
+              res.end(
+                JSON.stringify({ message: "File uploaded successfully" })
+              );
+            });
+          };
 
-                    this.routes[req.method.toLowerCase() + req.url](req,res);
-                }else{
-                    middleware_array[idx](req,res, ()=>{
-                        runMiddleware(req,res, middleware_array, idx+1);
-                    })
+          res.sendFile = async (path, mimeType) => {
+            const fileHandler = await fs.open(path, "r");
+            const readStream = fileHandler.createReadStream();
+
+            res.setHeader("content-type", mimeType);
+
+            readStream.pipe(res);
+          };
+
+          const urlWithoutParams = req.url.split("?")[0];
+          const params = req.url.split("?")[1];
+          req.params = new URLSearchParams(params); //params will include & so it will give params in an object
+
+          const runMiddleware = (req, res, middleware_array, idx) => {
+            if (idx === middleware_array.length) {
+              //if the routes obj does not have the key of req.method+req.url
+                if (!this.routes[req.method.toLowerCase() + urlWithoutParams]) {
+                    return res
+                    .status(404)
+                    .json({ error: `Cannot ${req.method} ${urlWithoutParams}` });
                 }
-            }
 
-            runMiddleware(req,res,this.middleware, 0)
+                this.routes[req.method.toLowerCase() + urlWithoutParams](
+                    req, 
+                    res,
+                    (error) => {
+                        res.setHeader("Connection", "close");
+                        this.handleErr(error, req, res);
+                    }
+                );
+            } else {
+              middleware_array[idx](req, res, () => {
+                runMiddleware(req, res, middleware_array, idx + 1);
+              });
+            }
+          };
+
+          runMiddleware(req, res, this.middleware, 0);
         });
     }
 
@@ -73,6 +91,10 @@ class Butter{
 
     beforeEach(cb){
         this.middleware.push(cb);
+    }
+
+    handleError(cb){
+        this.handleErr = cb;
     }
 
     listen (port, cb){
